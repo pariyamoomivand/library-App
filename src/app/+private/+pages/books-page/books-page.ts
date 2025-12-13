037cd10703a4117951b47a5d75fb95ab3c1cbb05
Pariya Moomivand <moomivandpariya4@gmail.com>
2025-12-13 10:39:38 +0330

    Create the appearance of the book page

--- src/app/+private/+pages/books-page/books-page.ts
+++ src/app/+private/+pages/books-page/books-page.ts
@@ -1,24 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BooksService } from './books-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-books-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './books-page.html',
   styleUrl: './books-page.scss',
 })
-export class BooksPage {
-  data:BookItem[]=[
-    {id:1,title:'برنامه نویسی',writer:'گاما',publisher:'_',price:8},
-    {id:2,title:'برنامه نویسی',writer:'گاما',publisher:'_',price:8},
-    {id:3,title:'برنامه نویسی',writer:'گاما',publisher:'_',price:8},
-    {id:4,title:'برنامه نویسی',writer:'گاما',publisher:'_',price:8},
-    {id:5,title:'برنامه نویسی',writer:'گاما',publisher:'_',price:8},
-  ];
+export class BooksPage implements OnInit{
+  booksService=inject(BooksService);
+  data: BookItem[] = [];
+  action:string='list';
+  item:BookItem={
+    id:0,
+    title:'',
+    writer:'',
+    publisher:'',
+    price:0
+  };
+  
+  ngOnInit(): void {
+    this.refreshData();
+  }
+  refreshData(){
+    this.data=this.booksService.list();
+  }
+  add(){
+    this.action='add';
+  }
+  save(){
+    this.booksService.add(this.item);
+    this.refreshData();
+    this.action='list';
+  }
+  cancel(){
+    this.action='list';
+  }
 }
 export interface BookItem {
-  id:number;
-  title:string;
-  writer:string;
-  publisher:string;
-  price:number;
+  id: number;
+  title: string;
+  writer: string;
+  publisher: string;
+  price: number;
 }
