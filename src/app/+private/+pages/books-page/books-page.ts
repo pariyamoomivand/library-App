3c6e5d2868d2f65ace2096d5da971310bc572904
Pariya Moomivand <moomivandpariya4@gmail.com>
2025-12-13 17:38:55 +0330

    Fixed the member page.

--- src/app/+private/+pages/books-page/books-page.ts
+++ src/app/+private/+pages/books-page/books-page.ts
@@ -1,41 +1,49 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { BooksService } from './books-service';
 import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-books-page',
-  imports: [FormsModule],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './books-page.html',
   styleUrl: './books-page.scss',
 })
-export class BooksPage implements OnInit{
-  booksService=inject(BooksService);
+export class BooksPage implements OnInit {
+  booksService = inject(BooksService);
   data: BookItem[] = [];
-  action:string='list';
-  item:BookItem={
-    id:0,
-    title:'',
-    writer:'',
-    publisher:'',
-    price:0
+  action: string = 'list';
+  item: BookItem = {
+    id: 0,
+    title: '',
+    writer: '',
+    publisher: '',
+    price: 0,
   };
-  
+
   ngOnInit(): void {
     this.refreshData();
   }
-  refreshData(){
-    this.data=this.booksService.list();
+  refreshData() {
+    this.data = this.booksService.list();
   }
-  add(){
-    this.action='add';
+  add() {
+    this.action = 'add';
+    this.item = {
+      id: 0,
+      title: '',
+      writer: '',
+      publisher: '',
+      price: 0,
+    };
   }
-  save(){
+  save() {
     this.booksService.add(this.item);
     this.refreshData();
-    this.action='list';
+    this.action = 'list';
   }
-  cancel(){
-    this.action='list';
+  cancel() {
+    this.action = 'list';
   }
 }
 export interface BookItem {
